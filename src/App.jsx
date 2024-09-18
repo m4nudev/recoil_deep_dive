77ab5f85e77fd8bc7abd66b908aa1be8bd4319ef
m4nudev <manudev912@gmail.com>
2024-09-19 00:44:19 +0530

    recoil(atom, selector)

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
+
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationAtomCount = useRecoilValue(notificationsAtom);
+  const MessagingAtomCount = useRecoilValue(messagingAtom);
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector)
+
+  return (
+    <>
+    <button>Home</button>
+  
+    <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+    <button>Jobs ({jobsAtomCount})</button>
+    <button>Messaging ({MessagingAtomCount})</button>
+    <button>Notifications ({notificationAtomCount})</button>
+ 
+    <button>Me ({totalNotificationCount})</button>
+    </>
+  )
+}
+
+export default App
